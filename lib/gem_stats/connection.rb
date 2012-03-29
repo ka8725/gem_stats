41ab2b6ab9f491dff2a7645f63252a5eba59f1b5
Andrey Koleshko <ka8725@gmail.com>
2012-03-30 02:48:13 +0300

    mongo connection

--- lib/gem_stats/connection.rb
+++ lib/gem_stats/connection.rb
@@ -1,8 +1,15 @@
+require 'erb'
+
 module GemStats
   module Connection
     def self.connect
-      MongoMapper.connection = Mongo::Connection.new('localhost')
-      MongoMapper.database = 'gem_stats'
+      env, path = defined?(Rails)? [Rails.env, [Rails.root, 'config/database.yml']] :
+        [(ENV['ENV'] || 'development'), File.expand_path("../../../config/database.yml", __FILE__)]
+      get_config(path) {|config| MongoMapper.setup(config, env) }
+    end
+
+    def self.get_config(*path)
+      yield YAML::load(ERB.new(File.read File.join(*path)).result)
     end
   end
 end
