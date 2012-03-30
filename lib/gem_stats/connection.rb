d6664838a3c527df5e6b9dc532e2626399398f08
Andrey Koleshko <ka8725@gmail.com>
2012-03-30 03:19:13 +0300

    fix connection

--- lib/gem_stats/connection.rb
+++ lib/gem_stats/connection.rb
@@ -2,14 +2,18 @@ require 'erb'
 
 module GemStats
   module Connection
-    def self.connect
-      env, path = defined?(Rails)? [Rails.env, [Rails.root, 'config/database.yml']] :
-        [(ENV['ENV'] || 'development'), File.expand_path("../../../config/database.yml", __FILE__)]
-      get_config(path) {|config| MongoMapper.setup(config, env) }
+    def self.connect(options = {})
+      if options.empty?
+        env, path = defined?(Rails)? [Rails.env, [Rails.root, 'config/database.yml']] :
+        [ENV['ENV'] || 'development', File.expand_path('../../../config/database.yml', __FILE__)]
+        get_config(path) {|config| MongoMapper.setup(config, env) }
+      else
+        MongoMapper.setup(options[:config], options[:env])
+      end
     end
 
     def self.get_config(*path)
-      yield YAML::load(ERB.new(File.read File.join(*path)).result)
+      yield YAML::load(ERB.new(File.read File.join(path)).result)
     end
   end
 end
